dadd78b86a26b5390a976b08f8781ed81ca67b82
Michael Ruocco <michael.ruocco@hotmail.com>
2017-11-10 12:22:55 +0000

    adds fake content loader and fake property loader for use when testing the real implementations

---
+++ src/test/java/uk/co/mruoc/properties/FakePropertyLoaderTest.java
@@ -0,0 +1,42 @@
+package uk.co.mruoc.properties;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FakePropertyLoaderTest {
+
+    private final FakePropertyLoader loader = new FakePropertyLoader();
+
+    @Test
+    public void lastLoadedPathShouldBeNullBeforeLoadPropertiesIsCalled() {
+        assertThat(loader.getLastLoadedPath()).isNull();
+    }
+
+    @Test
+    public void shouldReturnLastLoadedPath() {
+        String path = "/my/path";
+
+        loader.load(path);
+
+        assertThat(loader.getLastLoadedPath()).isEqualTo(path);
+    }
+
+    @Test
+    public void shouldReturnNullPropertiesIfNotSet() {
+        assertThat(loader.load("")).isNull();
+    }
+
+    @Test
+    public void shouldReturnProperties() {
+        Properties properties = new Properties();
+        loader.setProperties(properties);
+
+        Properties result = loader.load("");
+
+        assertThat(result).isEqualTo(properties);
+    }
+
+}
