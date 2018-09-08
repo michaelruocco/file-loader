f3714df1ecff512a54ee9a79be4c97a410e1d31e
Michael Ruocco <michael.ruocco@hotmail.com>
2018-09-08 12:43:52 +0100

    adds static content loader

---
+++ src/main/java/uk/co/mruoc/file/ContentLoader.java
@@ -0,0 +1,25 @@
+package uk.co.mruoc.file;
+
+public class ContentLoader {
+
+    public static String loadContentFromFileSystem(String path) {
+        final FileContentLoader loader = new FileSystemFileContentLoader();
+        return loader.loadContent(path);
+    }
+
+    public static String loadContentFromClasspath(String path) {
+        final FileContentLoader loader = new ClasspathFileContentLoader();
+        return loader.loadContent(path);
+    }
+
+    public static String loadBase64EncodedContentFromFileSystem(String path) {
+        final FileContentLoader loader = new FileSystemBase64FileContentLoader();
+        return loader.loadContent(path);
+    }
+
+    public static String loadBase64EncodedContentFromClasspath(String path) {
+        final FileContentLoader loader = new ClasspathBase64FileContentLoader();
+        return loader.loadContent(path);
+    }
+
+}
---
+++ src/test/java/uk/co/mruoc/file/ContentLoaderTest.java
@@ -0,0 +1,51 @@
+package uk.co.mruoc.file;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContentLoaderTest {
+
+    private static final String NEW_LINE = System.lineSeparator();
+
+    @Test
+    public void shouldReturnFileContentFromFileSystemFile() {
+        String expectedContent = "firstName=michael" + NEW_LINE + "surname=ruocco";
+        String path = "test/file-system.properties";
+
+        String content = ContentLoader.loadContentFromFileSystem(path);
+
+        assertThat(content).isEqualTo(expectedContent);
+    }
+
+    @Test
+    public void shouldReturnFileContentFromClasspathFile() {
+        String expectedContent = "firstName=MICHAEL" + NEW_LINE + "surname=RUOCCO";
+        String path = "/test/classpath.properties";
+
+        String content = ContentLoader.loadContentFromClasspath(path);
+
+        assertThat(content).isEqualTo(expectedContent);
+    }
+
+    @Test
+    public void shouldReturnBase64EncodedFileContentFromFileSystemFile() {
+        String expectedContent = "Zmlyc3ROYW1lPW1pY2hhZWwKc3VybmFtZT1ydW9jY28=";
+        String path = "test/file-system.properties";
+
+        String content = ContentLoader.loadBase64EncodedContentFromFileSystem(path);
+
+        assertThat(content).isEqualTo(expectedContent);
+    }
+
+    @Test
+    public void shouldReturnBase64EncodedFileContentFromClasspathFile() {
+        String expectedContent = "Zmlyc3ROYW1lPU1JQ0hBRUwKc3VybmFtZT1SVU9DQ08=";
+        String path = "/test/classpath.properties";
+
+        String content = ContentLoader.loadBase64EncodedContentFromClasspath(path);
+
+        assertThat(content).isEqualTo(expectedContent);
+    }
+
+}
